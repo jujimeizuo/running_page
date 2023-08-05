2314b1458d2687e1323d6ecfe3771712a495b450
jujimeizuo <fengzetao.zed@foxmail.com>
2023-08-05 17:19:11 +0800

    [feat] rebuild

---
+++ gatsby-config.js
@@ -0,0 +1,83 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+module.exports = {
+  pathPrefix: process.env.PATH_PREFIX || '/',
+  siteMetadata: {
+    siteTitle: '冯则涛的跑步记录',
+    siteUrl: 'https://running-page-rosy.vercel.app/',
+    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+    description: 'Personal site and blog',
+    navLinks: [
+      {
+        name: 'Blog',
+        url: 'http://www.jujimeizuo.cn/blog/',
+      },
+      {
+        name: 'About',
+        url: 'https://github.com/jujimeizuo/running_page/blob/master/README-CN.md',
+      },
+    ],
+  },
+  plugins: [
+    'gatsby-transformer-json',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
+
+    {
+      resolve: 'gatsby-plugin-vercel',
+      options: {
+        // (optional) Prints metrics in the console when true
+        debug: false,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/static/',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/images/',
+      },
+    },
+    {
+      resolve: 'gatsby-alias-imports',
+      options: {
+        rootFolder: '.',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        cssLoaderOptions: {
+          esModule: false,
+          modules: {
+            namedExport: false,
+          },
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#e1e1e1',
+        theme_color: '#e1e1e1',
+        display: 'standalone',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
+    },
+  ],
+};
