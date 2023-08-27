d0a48609b3033ab399b2cab6a9f7f073a34018f6
jujimeizuo <fengzetao.zed@foxmail.com>
2023-08-27 14:43:29 +0800

    [feat] add CNAME

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
     siteTitle: '冯则涛的跑步记录',
-    siteUrl: 'https://running-page-rosy.vercel.app/',
+    siteUrl: 'https://run.jujimeizuo.cn/',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
